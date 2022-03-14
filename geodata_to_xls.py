f95c969c5003924e0d6fe726fae881c667886af1
Hrvoje Sute <hsute@srce.hr>
2022-03-14 12:09:21 +0100

    dodane fi i lambda kolone

--- geodata_to_xls.py
+++ geodata_to_xls.py
@@ -3,8 +3,7 @@ import csv
 import pandas as pd
 
 CSV_DIR = 'csv/'
-CSV_OUTPUT = CSV_DIR + 'output/'
-CR_FILE = CSV_DIR + 'CivilReport.csv'
+CR_FILE = CSV_DIR + 'report.csv'
 CSV_DELIMITER = ';'
 
 
@@ -29,11 +28,15 @@ class GeodataExcelWriter:
     def add_sheet(self, sheet):
         df = pd.DataFrame(sheet.data)
         df.to_excel(self.xls_writer, sheet_name=self.name, startrow=4, index=False, header=False)
-        self.add_header()
+        self.format_document()
 
-    def add_header(self):
+    def format_document(self):
         workbook = self.xls_writer.book
         worksheet = self.xls_writer.sheets[self.name]
+
+        cell_format = workbook.add_format({
+            'align': 'center'
+        })
         header_format = workbook.add_format({
             'bold': True,
             'align': 'center',
@@ -41,6 +44,9 @@ class GeodataExcelWriter:
             'border': 1,
             'text_wrap': True
         })
+
+        worksheet.set_column(0, 5, 15, cell_format)
+        worksheet.set_column(6, 7, 20, cell_format)
         worksheet.merge_range('A1:H1', self.title, header_format)
         worksheet.merge_range('A3:A4', self.XLS_COL1, header_format)
         worksheet.merge_range('B3:B4', self.XLS_COL2, header_format)
@@ -53,9 +59,6 @@ class GeodataExcelWriter:
         worksheet.write(3, 6, self.XLS_COL7, header_format)
         worksheet.write(3, 7, self.XLS_COL8, header_format)
 
-        cell_format = workbook.add_format({'align': 'center'})
-        worksheet.set_column(0, 5, 15, cell_format)
-
     def done_writing(self):
         self.xls_writer.save()
 
@@ -63,42 +66,49 @@ class GeodataExcelWriter:
 class XlsSheet:
     def __init__(self, name):
         self.name = name
-        self.data = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []}
+        self.data = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: []}
 
     def add_row(self, csv_row):
-        for index, value in enumerate(csv_row):
+        eu_notion_row = self.convert_numbers(csv_row)
+        for index, value in enumerate(eu_notion_row):
             self.data[index].append(value)
 
-
-def convert_numbers(raw_row):
-    return [
-        row[0],
-        format(round(float(row[1]), 2), '.2f').replace('.', ','),
-        row[2],
-        format(round(float(row[3]), 2), '.2f').replace('.', ','),
-        format(round(float(row[4]), 2), '.2f').replace('.', ','),
-        format(round(float(row[5]), 2), '.2f').replace('.', ',')
-    ]
+    @staticmethod
+    def convert_numbers(raw_row):
+        return [
+            raw_row[0],
+            format(round(float(raw_row[1]), 2), '.2f').replace('.', ','),
+            raw_row[2],
+            format(round(float(raw_row[3]), 2), '.2f').replace('.', ','),
+            format(round(float(raw_row[4]), 2), '.2f').replace('.', ','),
+            format(round(float(raw_row[5]), 2), '.2f').replace('.', ','),
+            format(round(float(raw_row[6]), 13), '.13f').replace('.', ','),
+            format(round(float(raw_row[7]), 13), '.13f').replace('.', ',')
+        ]
 
 
 if __name__ == '__main__':
-    if not os.path.exists(CSV_OUTPUT):
-        os.mkdir(CSV_OUTPUT)
+    if not os.path.exists(CSV_DIR):
+        os.makedirs(GeodataExcelWriter.XLS_DIR)
 
-    with open(CR_FILE, newline='') as cr:
-        cr_reader = csv.reader(cr, delimiter=CSV_DELIMITER)
-        xls_sheet = None
-        geo_xls = None
+    try:
+        with open(CR_FILE, newline='') as cr:
+            cr_reader = csv.reader(cr, delimiter=CSV_DELIMITER)
+            xls_sheet = None
+            geo_xls = None
 
-        for row in cr_reader:
-            statio = row.pop(0)
-            if not geo_xls or (xls_sheet.name != statio):
-                if geo_xls:
-                    geo_xls.add_sheet(xls_sheet)
-                    geo_xls.done_writing()
+            for row in cr_reader:
+                statio = row.pop(0)
+                if statio == 'Station':
+                    continue
+                if not geo_xls or (xls_sheet.name != statio):
+                    if geo_xls:
+                        geo_xls.add_sheet(xls_sheet)
+                        geo_xls.done_writing()
 
-                geo_xls = GeodataExcelWriter(statio)
-                xls_sheet = XlsSheet(statio)
+                    geo_xls = GeodataExcelWriter(statio)
+                    xls_sheet = XlsSheet(statio)
 
-            conv_row = convert_numbers(row)
-            xls_sheet.add_row(conv_row)
+                xls_sheet.add_row(row)
+    except FileNotFoundError:
+        print('Nije pronadjena datoteka {}!'.format(CR_FILE))
