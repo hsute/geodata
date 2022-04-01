0c1e87b5a52511b382fcb5fb083047417ab82fba
Hrvoje Sute <hsute@srce.hr>
2022-04-01 06:03:08 +0200

    micanje loga

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ class GeodataApp(tk.Tk):
         super().__init__()
         self.title('GeodataApp')
         self.geometry("+600+300")
-        self.iconbitmap('img/logo.ico')
         self.river = ""
         self.tolerance = 0
         self.create_form()
