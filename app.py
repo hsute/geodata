cdf0330cd3123666a0aeaa113e77e233daafffd8
Hrvoje Sute <hsute@srce.hr>
2022-04-01 05:17:44 +0200

    lambda u command entryja

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from cls.statio_converter import Converter
 
 class GeodataApp(tk.Tk):
     label_msg = None
-    input_river = None
-    input_tolerance = None
 
     def __init__(self):
         super().__init__()
@@ -15,12 +13,12 @@ class GeodataApp(tk.Tk):
         self.tolerance = 0
         self.create_form()
 
-    def handle_submit(self):
-        if not self.validate():
+    def handle_submit(self, input_river, input_tolerance):
+        if not self.validate(input_river, input_tolerance):
             return
 
         # pokreni glavni racun
-        self.show_message("Pretvaram . . .", color="orange", width=36)
+        self.show_message("Pretvaram . . .", color="#e68c07", width=36)
         try:
             converter = Converter()
             ret = converter.run(self.river, self.tolerance)
@@ -30,14 +28,14 @@ class GeodataApp(tk.Tk):
         if ret == 0:
             self.show_message("Great success!", color="green")
 
-    def validate(self):
-        self.river = self.input_river.get()
+    def validate(self, input_river, input_tolerance):
+        self.river = input_river
         if self.river == "":
             self.show_message("Vodotok je nužan.")
             return False
-        if self.input_tolerance.get() != "":
+        if input_tolerance != "":
             try:
-                self.tolerance = float(self.input_tolerance.get())
+                self.tolerance = float(input_tolerance)
             except ValueError:
                 self.show_message("Tolerancija mora biti broj.")
                 return False
@@ -64,13 +62,13 @@ class GeodataApp(tk.Tk):
 
         label_river = tk.Label(master=form, text="Vodotok:", font=app_font)
         label_river.grid(row=0, column=0, sticky='e')
-        self.input_river = tk.Entry(master=form, width=24, font=app_font)
-        self.input_river.grid(row=0, column=1, sticky='w', padx=10)
+        input_river = tk.Entry(master=form, width=24, font=app_font)
+        input_river.grid(row=0, column=1, sticky='w', padx=10)
 
         label_tolerance = tk.Label(master=form, text="Tolerancija (m):", font=app_font)
         label_tolerance.grid(row=1, column=0, sticky='e')
-        self.input_tolerance = tk.Entry(master=form, width=8, font=app_font)
-        self.input_tolerance.grid(row=1, column=1, sticky='w', padx=10)
+        input_tolerance = tk.Entry(master=form, width=8, font=app_font)
+        input_tolerance.grid(row=1, column=1, sticky='w', padx=10)
 
         form.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
 
@@ -78,7 +76,7 @@ class GeodataApp(tk.Tk):
         self.label_msg.grid(row=1, column=0, columnspan=2, padx=10)
 
         button_run = tk.Button(master=self, text="Pokreni", font=('Arial', 16, 'bold'), bg='#0abf1c', fg='white',
-                               command=self.handle_submit)
+                               command=lambda: self.handle_submit(input_river.get(), input_tolerance.get()))
         button_run.grid(row=2, column=0, sticky='e', padx=10)
 
         tk.Button(master=self, text="Zatvori", font=app_font, command=self.destroy)\
